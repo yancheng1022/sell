172b1fb77ffa5f0931ba2d3426a984bb0d2ef00c
Richard <289799074@qq.com>
2018-11-18 18:57:50 +0800

    project init

---
+++ src/test/java/com/kaka/sell/SellApplicationTests.java
@@ -0,0 +1,23 @@
+package com.kaka.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SellApplicationTests {
+
+    @Test
+    public void contextLoads() {
+      log.debug("debug...");
+      log.error("error...");
+      log.info("info...");
+    }
+
+}
